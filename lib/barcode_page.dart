f67b397944a6dae19de1810f93ac172d2c184459
Teerasej <teerasej@gmail.com>
2025-03-07 23:42:37 +0700

    finish

---
+++ lib/barcode_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+import 'package:get/get.dart';
+import 'barcode_controller.dart';
+
+class BarcodePage extends StatelessWidget {
+  BarcodePage({super.key});
+
+  final BarcodeController barcodeController = Get.find();
+
+  Widget _buildBarcode(String value) {
+      if (value.isEmpty) {
+        return const Text(
+          'Scan something!',
+          overflow: TextOverflow.fade,
+          style: TextStyle(color: Colors.white),
+        );
+      }
+
+      barcodeController.barcodeValue.value = value;
+
+      return Text(
+        value,
+        overflow: TextOverflow.fade,
+        style: const TextStyle(color: Colors.white),
+      );
+    }
+
+    void _handleBarcode(BarcodeCapture barcodes) {
+      barcodeController.barcodeValue.value = barcodes.barcodes.firstOrNull?.displayValue ?? '';
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scan Barcode')),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: MobileScanner(
+                onDetect: _handleBarcode,
+              ),
+            ),
+            Center(child: Obx(() => _buildBarcode(barcodeController.barcodeValue.value))),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'barcode_controller.dart';
+
+class HomePage extends StatelessWidget {
+  var controller = Get.put(BarcodeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scan Barcode'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Get.toNamed('/scan');
+              },
+              child: Text('Scan Barcode'),
+            ),
+            Obx(() => Text(controller.barcodeValue.value),)
+          ],
+        ),
+      ),
+    );
+  }
+}
